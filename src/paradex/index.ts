617d37971ce8d5d571c03bd98631eac3be532722
xsfunc <xsfunc@gmail.com>
2025-03-30 20:13:23 +0300

    feat: add analyze module to paradex exports
    This commit introduces the `analyze` module to the `paradex` exports, enabling its usage alongside the existing API and helper functions. This change enhances the functionality of the `paradex` module by providing additional analysis capabilities.

--- src/paradex/index.ts
+++ src/paradex/index.ts
@@ -1,8 +1,10 @@
+import { analyze } from '#root/paradex/analyze.js'
 import { getMarketsBaseTokens } from '#root/paradex/api-helpers.js'
 import { api } from './api.js'
 
 export const paradex = {
   api,
+  analyze,
   helpers: {
     getMarketsBaseTokens,
   },
